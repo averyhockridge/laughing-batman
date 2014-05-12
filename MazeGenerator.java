45e3628aaed213b11d1b01d780652b5470ebb86b
brando2131 <brands2131@gmail.com>
2014-05-13 03:42:44 +1000

    Create MazeGenerator.java

---
+++ MazeGenerator.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ * @author Brandon Sandoval
+ *
+ */
+public class MazeGenerator {
+	
+	/**
+	 * 
+	 * @param size
+	 * @param width
+	 * @param height
+	 */
+	public MazeGenerator(int size, int width, int height){
+		
+	}
+	
+	/**
+	 * 
+	 */
+	public void randomiser(){
+		
+	}
+	
+	/**
+	 * 
+	 */
+	public void paint(){
+		
+	}
+	
+	/**
+	 * 
+	 * @param direction
+	 * @return
+	 */
+	public boolean canMove(int direction){
+		return true;
+	}
+}
