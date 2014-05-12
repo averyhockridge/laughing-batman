c148598fb981fe2124225763d4602549247c89fc
brando2131 <brands2131@gmail.com>
2014-05-13 03:42:13 +1000

    Create MazeGame.java

---
+++ MazeGame.java
@@ -0,0 +1,37 @@
+import java.awt.event.ActionEvent;
+
+/**
+ * 
+ * @author Brandon Sandoval
+ *
+ */
+
+public class MazeGame implements java.awt.event.ActionListener{
+	/**
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args){
+		
+	}
+	/**
+	 * 
+	 */
+	public void run(){
+		
+	}
+	/**
+	 * 
+	 * @param e
+	 */
+	public void ActionListener(ActionEvent e){
+
+	}
+	/**
+	 * 
+	 */
+	@Override
+	public void actionPerformed(ActionEvent e){
+		
+	}
+}
